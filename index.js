49592310af056fb6d004d520d144a9330c20a996
zain-coder-development <abeeshaabidi@gmail.com>
2023-01-22 15:18:15 +0530

    Fixed some bugs

---
+++ index.js
@@ -0,0 +1,13 @@
+const Eris = require("eris");
+const BOTTOKEN = require('./config.json').token;
+const bot = new Eris(BOTTOKEN);
+bot.on("ready", () => {
+  console.log("Ready!");
+});
+bot.on("messageCreate", (msg) => {
+  if(msg.content === "!ping") {
+    bot.createMessage(msg.channel.id, "Pong!");
+    
+  }
+});
+bot.connect();
